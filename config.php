b341a401fe3cf6cceebb12982aa5c666b16c0f4a
voltan <djvoltan@gmail.com>
2016-10-20 14:03:49 +0330

    Import theme

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+return array(
+    // Version
+    'version'       => '1.0.0',
+    // Type of layouts available in the theme
+    // Potential value: 'both', 'admin', 'front', default as 'both'
+    'type'          => 'front',
+    // Title of the theme
+    'title'         => 'Safand version 3',
+    // Author information: name, email, website
+    'author'        => 'Architecture: Hossein Azizabadi <azizabadi@faragostaresh.com>;',
+    // Screenshot image, relative path in asset.
+    // If no screenshot is available, static/image/screenshot.png will be used
+    'screenshot'    => 'image/screenshot.png',
+    // License or theme images and scripts
+    'license'       => 'Creative Common License'
+                     . ' http://creativecommons.org/licenses/by/3.0/',
+    // Optional description
+    'description'   => 'Safand theme',
+    // Parent theme from which templates can be inherited, default as 'default'
+    'parent'        => '',
+    // Supported browsers
+    'browser'       => 'Internet Explorer: 6+; Chrome: 17+; Firefox: 10+; Safari: 5.1+; Opera: 9.8+;',
+);
